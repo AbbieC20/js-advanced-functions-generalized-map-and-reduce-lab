4af8b5cc729818661bf18a239e3701e869a00ff9
Abbie Cornelius <abbyc@sky.com>
2021-10-05 20:57:03 +0100

    commit tree 83a8fd30ecd276c2ce4e0404b2b7f0abf7bfc0ed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map(array, callback) {
+    let UpdatedNumbers = [];
+
+    for (let i = 0; i < array.length; i++) {
+      const theElement = array[i];
+      UpdatedNumbers.push(callback(theElement));
+    }
+    return UpdatedNumbers;
+}
+
+function reduce(array, callback, startingPoint) {
+
+    let i = 0 
+    if (startingPoint === undefined) {
+        startingPoint = array[0]
+        i = 1
+    }
+
+    let value = startingPoint
+
+    for (i; i < array.length; i++) {
+        value = callback(array[i], value)
+    }
+    return value
+}
